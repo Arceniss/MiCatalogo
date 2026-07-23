edb9e7baea9a642d1588d5d98b137a8e32659193
Arceniss <arceniss@gmail.com>
2026-07-22 22:25:45 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'michaquetas-cache-v0.10';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar el Service Worker y almacenar archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  // Fuerza a que el SW se active inmediatamente
+  self.skipWaiting(); 
+});
+
+// Activar y limpiar cachés viejas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+// Estrategia Network-First (Busca en la red, si falla usa el caché)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
